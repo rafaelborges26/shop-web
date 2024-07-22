5c2c6b334eea18afd7529597a85e93f7543a7a53
Rafael Borges <rafael.borges2698@gmail.com>
2024-07-22 10:57:34 -0300

    feat - setup from project using stitches css

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,17 @@
+import { getCssText } from "@/styles";
 import { Html, Head, Main, NextScript } from "next/document";
+import Link from "next/link";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <Link rel="preconnect" href="https://fonts.googleapis.com" />
+      <Link rel="preconnect" href="https://fonts.gstatic.com" />
+      <Link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+
+      <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+      </Head>
       <body>
         <Main />
         <NextScript />
