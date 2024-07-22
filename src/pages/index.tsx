85d3fbcf19fbb4b62fdb66bf8c69e444132e8257
Rafael Borges <rafael.borges2698@gmail.com>
2024-07-22 18:19:18 -0300

    added page home using carousel with keen-slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,53 @@
 import { styled } from "@/styles";
+import { useKeenSlider,  } from 'keen-slider/react'
+import Image from "next/image";
+import { HomeContainer, Product } from "@/styles/pages/home";
+import camisa1 from '../assets/camisas/1.png'
+import camisa2 from '../assets/camisas/2.png'
+import camisa3 from '../assets/camisas/3.png'
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
+import 'keen-slider/keen-slider.min.css'
 
-  span: {
-    fontWeight: 'bold'
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-
-})
 
 export default function Home() {
+
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
+
   return (
-    <Button>
-      <span>teste</span>
-      Enviar</Button>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={camisa1} width={520} height={400} alt="camisa 1" />
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,00</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camisa2} width={520} height={400} alt="camisa 2"/>
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,00</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camisa3} width={520} height={400} alt="camisa 2"/>
+        <footer>
+          <strong>Camiseta 3</strong>
+          <span>R$ 79,00</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camisa3} width={520} height={400} alt="camisa 2"/>
+        <footer>
+          <strong>Camiseta 3</strong>
+          <span>R$ 79,00</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
