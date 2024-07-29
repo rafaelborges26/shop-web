758436f9049584d352907fbaacd3bf60ccce5c9d
Rafael Borges <rafael.borges2698@gmail.com>
2024-07-29 18:31:07 -0300

    feat- added redirect inside getServerSide if not exists param

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -33,7 +33,16 @@ export default function Success({ customerName, product }: SuccessProps ) {
 
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-    const sessionId = String(query.session_id)
+    const sessionId = query.session_id?.toString()
+
+    if(!sessionId){
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
 
     const session = await stripe.checkout.sessions.retrieve(sessionId, {
         expand: ['line_items', 'line_items.data.price.product'] //obter do produto
